e080f08bada03787bfd623984fc9e90598b1c6be
johnd3v <johntolentino1631@gmail.com>
2018-08-14 18:29:03 +0800

    Added function for register and login without OTP -john

--- app/src/main/java/com/example/tokenhubatu/logindraft2/LoginDraft2.java
+++ app/src/main/java/com/example/tokenhubatu/logindraft2/LoginDraft2.java
@@ -1,28 +1,105 @@
 package com.example.tokenhubatu.logindraft2;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class LoginDraft2 extends AppCompatActivity {
-   private Button btn;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Map;
+
+public class LoginDraft2 extends AppCompatActivity implements View.OnClickListener {
+   private Button btn,btnLogin;
+   private EditText txtPhone,txtPassword;
+
+   private DatabaseReference dbRef;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_draft2);
 
-         btn = (Button) findViewById(R.id.btnSignup);
+         btn =  findViewById(R.id.btnSignup);
+         btnLogin= findViewById(R.id.btnLogin);
 
-        btn.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-               openReg();
-            }
-        });
+         txtPhone=findViewById(R.id.txtPhone);
+         txtPassword=findViewById(R.id.txtPassword);
+
+
+        dbRef= FirebaseDatabase.getInstance().getReference().child("Users");
+
+        btn.setOnClickListener(this);
+        btnLogin.setOnClickListener(this);
+
+
     }
     public void openReg(){
         Intent intent = new Intent(this,Registration.class);
         startActivity(intent);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        if (view == btn){
+            openReg();
+        }
+        if (view == btnLogin){
+          final  String phoneNum=txtPhone.getText().toString().trim();
+          final String password=txtPassword.getText().toString().trim();
+            dbRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    if (TextUtils.isEmpty(txtPhone.getText().toString().trim())){
+                        txtPhone.setError("Please fill up phone number field");
+
+                    }else if(TextUtils.isEmpty(txtPassword.getText().toString().trim())) {
+                        txtPassword.setError("Please fill up password field");
+                    }else{
+
+                        boolean exists = false;
+                        for (DataSnapshot child : dataSnapshot.getChildren()) {
+                            Map<String, Object> model = (Map<String, Object>) child.getValue();
+
+                            if(model.get("password").equals(password) && model.get("phoneNumber").equals(phoneNum)) {
+                                exists = true;
+                                break;
+                            }
+                        }
+
+                        if(exists) {
+                          messageBox("found");
+
+                        }
+                        else {
+                            txtPassword.setError("Not found!");
+                            txtPhone.setError("Not found!");
+                        }
+                    }
+
+
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
+        }
+
+    }
+
+    public void messageBox(String s){
+        Toast.makeText(this,s,Toast.LENGTH_LONG).show();
     }
 }
--- app/src/main/java/com/example/tokenhubatu/logindraft2/Registration.java
+++ app/src/main/java/com/example/tokenhubatu/logindraft2/Registration.java
@@ -3,77 +3,132 @@ package com.example.tokenhubatu.logindraft2;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Handler;
+import android.os.PersistableBundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.PhoneNumberUtils;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.FirebaseTooManyRequestsException;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.concurrent.TimeUnit;
 
 
 public class Registration extends AppCompatActivity implements View.OnClickListener {
     EditText etPhone,etPass1,etpass2;
     Button btnreg, btnsignUp;
-    private FirebaseAuth mAuth;
+
     private ProgressDialog progressDialog;
 
+    private static final String TAG = "Registration";
+    private String mVerificationId;
 
+    private DatabaseReference dbRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
-        mAuth = FirebaseAuth.getInstance();
-
 
 
         progressDialog = new ProgressDialog(this);
 
-        etPhone = (EditText) findViewById(R.id.etPhoneNum);
-        etPass1 = (EditText) findViewById(R.id.etPass1);
-        etpass2 = (EditText) findViewById(R.id.etPass2);
-        btnreg = (Button) findViewById(R.id.btnreg);
-        btnsignUp = (Button)findViewById(R.id.btnsignUp);
+        etPhone = findViewById(R.id.etPhoneNum);
+        etPass1 = findViewById(R.id.etPass1);
+        etpass2 = findViewById(R.id.etPass2);
+        btnreg = findViewById(R.id.btnreg);
+        btnsignUp = findViewById(R.id.btnsignUp);
 
-//        btnreg.setOnClickListener(new View.OnClickListener()
         btnreg.setOnClickListener(this);
         btnsignUp.setOnClickListener(this);
 
+        dbRef=FirebaseDatabase.getInstance().getReference().child("Users");
 
     }
     public void displayToast(View v) {
         Toast.makeText(this, "Wrong Password", Toast.LENGTH_SHORT).show();
     }
 
+    public void messageBox(String s){
+        Toast.makeText(this,s,Toast.LENGTH_LONG).show();
+    }
 
     private void RegUser(){
         String Pnumber = etPhone.getText().toString().trim();
-        String Fpass = etpass2.getText().toString().trim();
-
+        String confirmPass = etpass2.getText().toString().trim();
+        String Fpass= etPass1.getText().toString().trim();
         if (TextUtils.isEmpty(Pnumber)){
-            Toast.makeText(this,"Enter Phone Number", Toast.LENGTH_SHORT).show();
+        etPhone.setError("Cannot be empty!");
+
             return ;
         }
         if (TextUtils.isEmpty(Fpass)){
-            Toast.makeText(this,"Enter Password", Toast.LENGTH_SHORT).show();
+           etPass1.setError("Cannot be empty!");
             return;
         }
-        progressDialog.setMessage("Registering User..");
-        progressDialog.show();
+
+        if (TextUtils.isEmpty(confirmPass)){
+          etpass2.setError("Cannot be empty");
+            return;
+        }
+        if (confirmPass.equals(Fpass)){
+                User user= new User(Pnumber,Fpass);
+
+            if (PhoneNumberUtils.isGlobalPhoneNumber(Pnumber)){
+
+                   progressDialog.setMessage("Registering User..");
+                   progressDialog.show();
+
+
+                   dbRef.push().setValue(user, new DatabaseReference.CompletionListener(){
+
+                    @Override
+                    public void onComplete(DatabaseError databaseError, DatabaseReference databaseReference) {
+                        progressDialog.dismiss();
+                        messageBox("User Registered");
+//                        Intent i= new Intent(this,LoginDraft2.class    );
+                        Intent intent = new Intent(Registration.this,LoginDraft2.class);
+                        startActivity(intent);
+                    }
+                });
+            }else{
+                etPhone.setError("Invalid Phone Number");
+            }
+        }else{
+            etPass1.setError("Password not matched!");
+            etpass2.setError("Password not matched!");
+        }
+
+
 
     }
-    public void onClick(View v){
-        if(v == btnreg){
+
+
+    @Override
+    public void onClick(View view) {
+        if (view == btnreg){
             RegUser();
         }
-        if (v == btnsignUp){
-
-        }
     }
-
 }
